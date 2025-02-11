3cd5158f0508fc8fe782dc67230dc62f23bfacf2
MillionMedia2 <neil@millionmedia.com>
2025-02-11 20:40:52 +0000

    Update chat-widget.js

--- chat-widget.js
+++ chat-widget.js
@@ -401,6 +401,15 @@
        }
    }
 
+    function generateUUIDFallback() {
+    let dt = new Date().getTime();
+    const uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        const r = (dt + Math.random() * 16) % 16 | 0;
+        dt = Math.floor(dt / 16);
+        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+    return uuid;
+}
     async function startNewConversation() {
         currentSessionId = generateUUID();
         const data = [{
